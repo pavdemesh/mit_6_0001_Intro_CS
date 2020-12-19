ea1bd6b49cce4b162d533640e8358007132bc902
pavdemesh <pavdemesh@gmail.com>
2020-12-19 11:38:04 +0200

    done except p6

--- PSet_3/ps3.py
+++ PSet_3/ps3.py
@@ -381,7 +381,7 @@ def play_game(word_list):
     * For each hand, before playing, ask the user if they want to substitute
       one letter for another. If the user inputs 'yes', prompt them for their
       desired letter. This can only be done once during the game. Once the
-      substitue option is used, the user should not be asked if they want to
+      substitute option is used, the user should not be asked if they want to
       substitute letters in the future.
 
     * For each hand, ask the user if they would like to replay the hand.
